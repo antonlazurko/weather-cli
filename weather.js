48918952ac9d411418c1f02b8e3da29a93734a93
Anton Lazurko <antla@ciklum.com>
2024-11-12 11:47:23 +0200

    feat: add args parser

--- weather.js
+++ weather.js
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
+import { getArgs } from './helpers/args.js';
 
 const initCLI = (params) => {
     console.log('Initializing CLI');
-    console.log('process.a :>> ', process.argv);
+    const args = getArgs(process.argv);
+    console.log('args :>> ', args);
+    if(args.help) {
+        console.log('Help');
+    }
+    if(args.city) {
+        console.log('city');
+    }
+    if(args.token) {
+        console.log('token');
+    }
 }
 
 initCLI()
